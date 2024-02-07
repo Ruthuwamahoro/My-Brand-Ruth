93c094722ef809a761137bc51bd41c665fd3821d
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-02-07 13:54:16 +0200

    remove files

--- UI/admin/activities.js
+++
@@ -1,6 +0,0 @@
-let users = JSON.parse(localStorage.getItem("StoreUsers"));
-if (!users || !users.isAuthenticated) {
-    window.location.href = "../login-page/login.html";
-}
-
-
--- UI/admin/notifications.js
+++
@@ -1,5 +0,0 @@
-//authentication to this page
-let users = JSON.parse(localStorage.getItem("StoreUsers"));
-if (!users || !users.isAuthenticated) {
-    window.location.href = "../login-page/login.html";
-}
