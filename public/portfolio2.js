3c64f273cea0fc3dbc8f4071340e2c84759463e3
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2024-03-05 23:41:19 +0200

    displaying blogs on portfolio

--- public/portfolio2.js
+++ public/portfolio2.js
@@ -59,15 +59,94 @@ submission.addEventListener("click", (e) => {
     }
     fetchingData();
 });
-// fetch("http://localhost:8080/contact/contactmessage", {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//         name: inputName.value,
-//         email: inputEmail.value,
-//         message: inputMessage.value
-//     })
-// }).then((result) => result.json()).then((data) => console.log(data))
+function displayBlog() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const token = JSON.parse(localStorage.getItem('token') || '{}');
+        const getToken = token.token;
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        headers.append('Authorization', `Bearer ${getToken}`);
+        const customization = {
+            method: 'GET',
+            headers: headers
+        };
+        try {
+            const response = yield fetch('https://brand-backend-side.onrender.com/post/retrieveallpost', customization);
+            const blogs = yield response.json();
+            console.log(blogs.data[0]);
+            const blog = blogs.data;
+            if (blog[0] && blog[1]) {
+                const getContainer = document.querySelector('.container-blog.blog1');
+                let makeNewBlogArr = [];
+                const firstBlo = blog[0];
+                const secondBlo = blog[1];
+                makeNewBlogArr.push(firstBlo, secondBlo);
+                let elem = '';
+                makeNewBlogArr.forEach((rec) => {
+                    elem += `
+                <div>
+                    <h1>${rec.title}</h1>
+                    <p>${rec.description}</p>
+                    <div id="read-more">
+                        <a href="" style="margin-left: 30px;">Read more</a>
+                        <a href=""><i class="fa-solid fa-circle-right"></i></a>
+                    </div>
+                </div>
+    
+                `;
+                });
+                getContainer.innerHTML = elem;
+            }
+            if (blog[2] && blog[3]) {
+                const getContainer = document.querySelector('.container-blog.blog2');
+                let makeNewBlogArr = [];
+                const firstBlo = blog[2];
+                const secondBlo = blog[3];
+                makeNewBlogArr.push(firstBlo, secondBlo);
+                let elem = '';
+                makeNewBlogArr.forEach((rec) => {
+                    elem += `
+                <div>
+                    <h1>${rec.title}</h1>
+                    <p>${rec.description}</p>
+                    <div id="read-more">
+                        <a href="" style="margin-left: 30px;">Read more</a>
+                        <a href=""><i class="fa-solid fa-circle-right"></i></a>
+                    </div>
+                </div>
+    
+                `;
+                });
+                getContainer.innerHTML = elem;
+            }
+            if (blog[4] && blog[5]) {
+                const getContainer = document.querySelector('.container-blog.blog3');
+                let makeNewBlogArr = [];
+                const firstBlo = blog[4];
+                const secondBlo = blog[5];
+                makeNewBlogArr.push(firstBlo, secondBlo);
+                console.log(makeNewBlogArr);
+                let elem = '';
+                makeNewBlogArr.forEach((rec) => {
+                    elem += `
+                <div>
+                    <h1>${rec.title}</h1>
+                    <p>${rec.description}</p>
+                    <div id="read-more">
+                        <a href="" style="margin-left: 30px;">Read more</a>
+                        <a href=""><i class="fa-solid fa-circle-right"></i></a>
+                    </div>
+                </div>
+    
+                `;
+                });
+                getContainer.innerHTML = elem;
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+}
+displayBlog();
